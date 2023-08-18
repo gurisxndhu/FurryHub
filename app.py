978f27cc79fd6e9a2d80bbf9e38f7504123a9002
Gurinder Singh Sandhu <133472518+gurisxndhu@users.noreply.github.com>
2023-08-17 21:34:32 -0400

    Add files via upload

---
+++ app.py
@@ -0,0 +1,26 @@
+import numpy as np
+from flask import Flask, render_template,request
+
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+@app.route("/about-team")
+def team():
+    return render_template("about-team.html")
+
+@app.route("/dashboard")
+def dashboard():
+    return render_template("dashboard.html")
+
+@app.route("/project")
+def project():
+    return render_template("project.html")
+
+
+if __name__ == "__main__":
+    from waitress import serve
+    serve(app, host="0.0.0.0", port=8080)
